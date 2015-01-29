7e89545ec521da8ccc6e8d05f7a5216186a8da32
ashkoruta <ashkoruta@gmail.com>
2015-01-29 19:04:51 +0300

    supposedly works
    4 motors and stepping

--- HRM_Firmware/HRM_Firmware.ino
+++ HRM_Firmware/HRM_Firmware.ino
@@ -5,9 +5,24 @@
 #include "utility/Adafruit_PWMServoDriver.h"
 
 #define _DBG_
+void dbgPrint(String str)
+{
+#ifdef _DBG_
+	Serial.println(str);
+#endif
+}
+void dbgPrint(int i)
+{
+#ifdef _DBG_
+	Serial.println(i);
+#endif
+}
 // by definition of communications we have 4 integers for coordinates 7 bytes each
-const int NumberOfCoordinates = 4;
 const int BytesInNumber = 7; // why 7?
+// section of motors' coordinates defines
+enum Coordinates { X0, Y0, X1, Y1 };
+const int NumberOfCoordinates = 4;
+long currentCoordinates[NumberOfCoordinates] = {};
 
 // plane 0 is controlled by 2 motors (X, Y)
 #define PLANE_0 0
@@ -17,30 +32,80 @@ const int BytesInNumber = 7; // why 7?
 enum State { IDLE, MOVING, HOMING }; //HOMING is still to be used
 State CncState = IDLE;
 
-// section of motors' coordinates defines
-long currentCoordinates[NumberOfCoordinates] = {};
-const int MotorsCount = 1; //TODO will be NumberOfCoordinates
-
 // motors instantiation
 // motor shield object with the default I2C address
 Adafruit_MotorShield AdaShields[] = { 
 	Adafruit_MotorShield(0x60),
     Adafruit_MotorShield(0x61)
- };
+};
 
 // Connect a stepper motors with 200 steps per revolution (1.8 degree)
 Adafruit_StepperMotor* Motors[NumberOfCoordinates] = {
-	//AdaShields[PLANE_0].getStepper(200, 1), // to motor port #1 (M1 and M2) on first shield, X0
+	AdaShields[PLANE_0].getStepper(200, 1), // to motor port #1 (M1 and M2) on first shield, X0
 	AdaShields[PLANE_0].getStepper(200, 2), // to motor port #2 (M3 and M4) on first shield, Y0
-	AdaShields[PLANE_1].getStepper(200, 1) // and to motor port #1 (M3 and M4) on second shield, X1
-	//AdaShields[PLANE_1].getStepper(200, 2) // and to motor port #1 (M3 and M4) on second shield, Y1
+	AdaShields[PLANE_1].getStepper(200, 1), // and to motor port #1 (M3 and M4) on second shield, X1
+	AdaShields[PLANE_1].getStepper(200, 2) // and to motor port #1 (M3 and M4) on second shield, Y1
 };
-void dbgPrint(String str)
+// this thing wraps around all motors and moving actions
+void forwardstepX0()
 {
-#ifdef _DBG_
-	Serial.println(str);
-#endif
+	Motors[X0]->onestep(FORWARD, SINGLE);
 }
+void backwardstepX0()
+{
+	Motors[X0]->onestep(BACKWARD, SINGLE);
+}
+void forwardstepY0()
+{
+	Motors[Y0]->onestep(FORWARD, SINGLE);
+}
+void backwardstepY0()
+{
+	Motors[Y0]->onestep(BACKWARD, SINGLE);
+}
+void forwardstepX1()
+{
+	Motors[X1]->onestep(FORWARD, SINGLE);
+}
+void backwardstepX1()
+{
+	Motors[X1]->onestep(BACKWARD, SINGLE);
+}
+void forwardstepY1()
+{
+	Motors[Y1]->onestep(FORWARD, SINGLE);
+}
+void backwardstepY1()
+{
+	Motors[Y1]->onestep(BACKWARD, SINGLE);
+}
+AccelStepper stepper[] = {
+	AccelStepper(forwardstepX0, backwardstepX0),
+	AccelStepper(forwardstepY0, backwardstepY0),
+	AccelStepper(forwardstepX1, backwardstepX1),
+	AccelStepper(forwardstepY1, backwardstepY1)
+};
+
+void MoveEveryThing()
+{
+	dbgPrint("actual movement by steps");
+	boolean needMoreMovement = false;
+	long delta[NumberOfCoordinates] = {};
+	for (int i = 0; i < NumberOfCoordinates; ++i) {
+		delta[i] = stepper[i].distanceToGo(); // calculate for each motor
+		if (delta[i]) { // if he still needs to step, do it
+			dbgPrint("step of");
+			dbgPrint(i);
+			stepper[i].run();
+			needMoreMovement = needMoreMovement || true;
+		}
+	}
+	// we didn't make a single step we must have finished
+	if (!needMoreMovement) {
+		CncState = IDLE;
+		Serial.println("Success"); // TODO: write status line here
+	}
+};
 void setup()
 {
 	Serial.begin(9600);           // set up Serial library at 9600 bps
@@ -48,9 +113,9 @@ void setup()
 	for (int i = 0; i < NumberOfCoordinates; ++i) {
 		currentCoordinates[i] = 0;
 	}
-	dbgPrint("before first shield");
+	dbgPrint("init first shield");
 	AdaShields[PLANE_0].begin();
-	dbgPrint("before second shield");
+	dbgPrint("init second shield");
 	AdaShields[PLANE_1].begin();
 	dbgPrint("Ready for work");
 }
@@ -63,7 +128,7 @@ int messageLengthReceived = 0;
 // moves all content in buffer one position forward
 void shiftBuffer(char *buffer)
 {
-	Serial.println("shift buffer");
+	dbgPrint("shift buffer");
 	for (int i = 1; i < BUFFSIZE; i++) {
 		buffer[i - 1] = buffer[i];
 	}
@@ -90,7 +155,6 @@ int checkSerial()
   }
   return 0; // no message received yet
 }
-
 long parseUint(char *str, int n)
 {
 	assert(n == BytesInNumber);
@@ -100,43 +164,16 @@ long parseUint(char *str, int n)
 	}
 	return res;
 }
-
 // 1 if success, 0 if error
 void parseCoordinates(unsigned long *coordinates)
 {
+	dbgPrint("parse coordinates");
 	// first symbol should be 'G' - if it's not we shouldn't be here at first place
 	assert(msgbuf[0] == 'G');
 	for (int i = 0; i < NumberOfCoordinates; ++i) {
 		coordinates[i] = parseUint(msgbuf + BytesInNumber * i, BytesInNumber);
 	}
 }
-// this thing wraps around all motors and moving actions
-void forwardstepY0() { Motors[0]->onestep(FORWARD, SINGLE); }
-void backwardstepY0() { Motors[0]->onestep(BACKWARD, SINGLE); }
-void forwardstepX1() { Motors[1]->onestep(FORWARD, SINGLE); }
-void backwardstepX1() { Motors[1]->onestep(BACKWARD, SINGLE); }
-AccelStepper stepperY0(forwardstepY0, backwardstepY0);
-AccelStepper stepperX1(forwardstepX1, backwardstepX1);
-
-void MoveEveryThing()
-{
-	int needMoreMoves = 0;
-	// TODO stepperX1
-	while (stepperY0.distanceToGo() > 0) {
-		needMoreMoves++;
-		stepperY0.run();
-	}
-	while (stepperX1.distanceToGo() > 0) {
-		needMoreMoves++;
-		stepperX1.run();
-	}
-	// TODO stepperY1
-	if (needMoreMoves == 0) {
-		CncState = IDLE;
-		Serial.println("Success"); // TODO: write status line here
-	}
-};
-
 enum CommandType { CT_NOT_VALID, CT_MOVE, CT_HOME, CT_TEMPERATURE, CT_RELEASING };
 class Command
 {
@@ -185,14 +222,14 @@ public:
 			// move motors
 			CncState = MOVING;
 			// change one coordinate by one exact motor
-			//stepperX0
-			stepperY0.move(this->_coordinates[0] - currentCoordinates[0]);
-			stepperX1.move(this->_coordinates[1] - currentCoordinates[1]);
-			//stepperY1
-			for (int i = 0; i < NumberOfCoordinates; ++i)
+			dbgPrint("moving of motors");
+			for (int i = 0; i < NumberOfCoordinates; ++i) {
+				stepper[i].move(this->_coordinates[i] - currentCoordinates[i]);
+				//TODO set speeds
 				currentCoordinates[i] = this->_coordinates[i];
+			}
 			break;
-		//TODO others
+		//TODO other commands
 		default:
 			return;
 		}
@@ -201,9 +238,6 @@ public:
 
 void loop()
 {
-	Serial.println("TEST3");
-	delay(1000);
-	return;
 	if (checkSerial() == 0) {
 		if (CncState == IDLE) {
 			static int idleLoopCounter = 0;
@@ -215,11 +249,11 @@ void loop()
 		}
 	} else {
 		// parse command
-		// TODO: add workaround if first symbol is not in the list, but message was successfully received
 		Command cmd = Command::getCommand(msgbuf);
 		if (cmd.type() == CT_NOT_VALID) {
 			Serial.println("!Error: check your command type");
 		} else {
+			dbgPrint("set environment for command");
 			cmd.setEnvVariables();
 		}
 	}
